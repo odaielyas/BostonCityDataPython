9b189b7b57afcd0f9cd13cf8446ef9ef9aa1b6c5
Odai Elyas <77697922+odaielyas@users.noreply.github.com>
2023-02-15 15:53:52 +0000

    save changes

--- boston.py
+++ boston.py
@@ -1,13 +1,23 @@
 #Read json from url and print data
+#run notebook for graphing .ipynb
 import json
 import urllib.request
 import urllib.parse
 import urllib.error
 
-url = input('https://pollysnips.s3.amazonaws.com/bostonEmployeeSalaries.json')
+url = "https://pollysnips.s3.amazonaws.com/bostonEmployeeSalaries.json"
 print('Retrieving', url)
 uh = urllib.request.urlopen(url)
 data = uh.read().decode()
 print('Retrieved', len(data), 'characters')
+print(type(data))
 info = json.loads(data)
+
+js = json.loads(data)
+print(type(js))
+
+data = js["data"]
+print(type(data))
+print(data[0])
+#meta = js["meta"]
 
